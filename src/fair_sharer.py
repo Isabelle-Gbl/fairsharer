e66b6614e36d0ccf9c327f02ea035610f76c2a02
Isabelle-Gbl <148323279+Isabelle-Gbl@users.noreply.github.com>
2023-11-26 16:55:46 +0100

    Update fair_sharer.py
    unnecessary deleted

--- src/fair_sharer.py
+++ src/fair_sharer.py
@@ -10,12 +10,4 @@ def fair_sharer(values, num_iterations, share=0.1):
         values[max_index+1] = values[max_index+1] + share_value
         values[max_index] = max_value - (2 * share_value)
       
-    #print(values)
     return values
-    
-
-
-
-
-#fair_sharer([0, 1000, 800, 0], 1) # --> [100, 800, 900, 0]
-#fair_sharer([0, 1000, 800, 0], 2) # --> [100, 890, 720, 90]

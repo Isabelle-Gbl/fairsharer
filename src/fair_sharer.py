69cc12e671ccf8e737c9015c58925bcb2d8be659
Isabelle-Gbl <148323279+Isabelle-Gbl@users.noreply.github.com>
2023-11-26 14:59:21 +0100

    Update fair_sharer.py
    first try fair_sharer method

--- src/fair_sharer.py
+++ src/fair_sharer.py
@@ -1,1 +1,21 @@
+def fair_sharer(values, num_iterations, share=0.1):
 
+    for i in range(num_iterations):
+
+        max_value = max(values)
+        max_index = values.index(max_value)
+
+        share_value = max_value * share
+        values[max_index-1] = values[max_index-1] + share_value
+        values[max_index+1] = values[max_index+1] + share_value
+        values[max_index] = max_value - (2 * share_value)
+      
+    #print(values)
+    return values
+    
+
+
+
+
+#fair_sharer([0, 1000, 800, 0], 1) # --> [100, 800, 900, 0]
+#fair_sharer([0, 1000, 800, 0], 2) # --> [100, 890, 720, 90]
